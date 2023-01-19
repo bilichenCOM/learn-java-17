e34c728e9317f7965d57af235f100a30253d0a7f
mbilichenko <bilichenko.mykhailo@gmail.com>
2023-01-19 10:32:11 +0200

    bound and unbound method references;

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world from java 17!");
+        String first = "hello";
+        String second = "good morning";
+
+        BinaryOperator<String> concat = String::concat;
+        UnaryOperator<String> makeUpperCase = String::toUpperCase;
+
+
+        concat.andThen(makeUpperCase);
     }
+
+
 }
